54531e9a63d52ea4761cd378437d06bd4d301588
kdnhyk <kdnhyk@gmail.com>
2024-05-20 15:30:20 +0900

    feat: packages/ui

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,35 @@
-"use client";
+import { motion } from 'framer-motion'
 
-import { ReactNode } from "react";
+const Button = ({
+  icon,
+  children,
+  onClick,
+  disable,
+  color = 'white',
+}: {
+  icon?: React.ReactNode
+  children: React.ReactNode
+  onClick: () => void
+  disable?: boolean
+  color?: 'black' | 'white'
+}) => {
+  return (
+    <motion.button
+      className={`text-14 relative flex h-8 w-fit items-center justify-between gap-1 rounded-[8px] border border-grey-700 px-3 transition-colors disabled:cursor-not-allowed disabled:bg-white disabled:text-grey-900 ${color === 'black' ? 'bg-[#000] text-white' : 'bg-white text-[#000]'}`}
+      disabled={disable}
+      onClick={onClick}
+      whileTap={{
+        scale: 0.95,
+        opacity: 0.8,
+      }}
+    >
+      {icon && <div className="flex items-center justify-center">{icon}</div>}
 
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+      <p>{children}</p>
+
+      {icon && <div />}
+    </motion.button>
+  )
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
-  );
-};
+export { Button }
