66967679d0ea32f73cc2344afeb99c4c2c6a90b3
kdnhyk <kdnhyk@gmail.com>
2024-05-20 15:25:04 +0900

    feat: packages/api

---
+++ packages/api/src/auth-app/index.ts
@@ -0,0 +1,77 @@
+import { User } from '@supabase/supabase-js'
+import { supabase } from '../supabase-app'
+
+const signin = {
+  signInWithKakao: async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'kakao',
+      options: {
+        redirectTo: window.location.origin + '/auth',
+      },
+    })
+
+    if (error) {
+      console.log(error)
+    }
+  },
+
+  signInWithGoogle: async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: window.location.origin + '/auth',
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+      },
+    })
+
+    if (error) {
+      console.log(error)
+    }
+  },
+
+  signInWithApple: async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'apple',
+      options: {
+        redirectTo: window.location.origin + '/auth',
+      },
+    })
+
+    if (error) {
+      console.log(error)
+    }
+  },
+}
+
+const signout = async () => {
+  const { error } = await supabase.auth.signOut()
+
+  if (error) {
+    console.log(error)
+  }
+}
+
+const getUser: () => Promise<User | null> = async () => {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user) return null
+
+  return user
+}
+
+const getUserId: () => Promise<string | null> = async () => {
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (!session) return null
+
+  return session.user.id
+}
+
+export { signin, signout, getUser, getUserId }
---
+++ packages/api/src/user/profile.ts
@@ -0,0 +1,53 @@
+import { getUserId } from '../auth'
+import { supabase } from '../supabase'
+import { ProfileType } from '@repo/types/profile'
+
+const PROFILE_SELECOR = `user_id, profile_image, nickname`
+
+const getProfile = async () => {
+  const user_id = await getUserId()
+
+  let { data, error } = await supabase
+    .from('profile')
+    .select(PROFILE_SELECOR)
+    .eq('user_id', user_id)
+    .limit(1)
+    .maybeSingle()
+
+  if (error) {
+    console.log(error)
+  }
+
+  return data as ProfileType | null
+}
+
+const postProfile = async (profile: ProfileType) => {
+  const { data, error } = await supabase
+    .from('profile')
+    .insert([profile])
+    .select()
+    .maybeSingle()
+
+  if (error) {
+    console.log(error)
+  }
+}
+
+const updateProfile = async (profile: Partial<ProfileType>) => {
+  const user_id = await getUserId()
+
+  const { data, error } = await supabase
+    .from('profile')
+    .update([{ user_id, ...profile }])
+    .eq('user_id', user_id)
+    .select(PROFILE_SELECOR)
+    .maybeSingle()
+
+  if (error) {
+    console.log(error)
+  }
+
+  return data as ProfileType
+}
+
+export { getProfile, postProfile, updateProfile }
