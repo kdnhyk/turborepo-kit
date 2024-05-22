4c86e6a650cc4ead162ed5e94bd9f78a34f0410d
kdnhyk <kdnhyk@gmail.com>
2024-05-22 20:29:59 +0900

    feat: Add AppleAuth

---
+++ apps/app/components/auth/AppleAuth.tsx
@@ -0,0 +1,57 @@
+import { Platform, useColorScheme } from 'react-native'
+import * as AppleAuthentication from 'expo-apple-authentication'
+import { supabase } from '@repo/api/supabase'
+
+const AppleAuth = () => {
+  const colorScheme = useColorScheme()
+
+  if (Platform.OS === 'ios')
+    return (
+      <AppleAuthentication.AppleAuthenticationButton
+        buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
+        buttonStyle={
+          colorScheme === 'dark'
+            ? AppleAuthentication.AppleAuthenticationButtonStyle.WHITE
+            : AppleAuthentication.AppleAuthenticationButtonStyle.BLACK
+        }
+        cornerRadius={5}
+        style={{ width: '100%', height: 48 }}
+        onPress={async () => {
+          try {
+            const credential = await AppleAuthentication.signInAsync({
+              requestedScopes: [
+                AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+                AppleAuthentication.AppleAuthenticationScope.EMAIL,
+              ],
+            })
+
+            // Sign in via Supabase Auth.
+            if (credential.identityToken) {
+              const {
+                error,
+                data: { user },
+              } = await supabase.auth.signInWithIdToken({
+                provider: 'apple',
+                token: credential.identityToken,
+              })
+              console.log(JSON.stringify({ error, user }, null, 2))
+              if (!error) {
+                // User is signed in.
+                console.log(user)
+              }
+            } else {
+              throw new Error('No identityToken.')
+            }
+          } catch (e: any) {
+            if (e.code === 'ERR_REQUEST_CANCELED') {
+              // handle that the user canceled the sign-in flow
+            } else {
+              // handle other errors
+            }
+          }
+        }}
+      />
+    )
+  return <>{/* Implement Android Auth options. */}</>
+}
+export { AppleAuth }
