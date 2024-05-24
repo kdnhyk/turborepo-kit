323de3a621a10e67580fefb30edd327ac3133761
kdnhyk <kdnhyk@gmail.com>
2024-05-25 00:26:07 +0900

    mod: Change UserProfile

--- apps/web/app/(components)/user/UserProfile.tsx
+++ apps/web/app/(components)/user/UserProfile.tsx
@@ -6,6 +6,8 @@ import Link from 'next/link'
 const UserProfile = () => {
   const { profile, resetProfile } = useProfile()
 
+  
+
   const logout = () => {
     signout()
     resetProfile()
@@ -13,18 +15,27 @@ const UserProfile = () => {
 
   return (
     <>
-      <div className="flex flex-col gap-2">
-        <p>{`안녕하세요, ${profile.data?.nickname}님`}</p>
-        <div className="flex flex-col items-end gap-2">
-          <Link className="underline" href="/posts">
-            글 목록
-          </Link>
-          <Link className="underline" href="/profile">
-            프로필
-          </Link>
-          <Button onClick={logout}>로그아웃</Button>
+      {profile.data ? (
+        <div className="flex flex-col gap-2">
+          <p>{`안녕하세요, ${profile.data?.nickname}님`}</p>
+          <div className="flex flex-col items-end gap-2">
+            <Link className="underline" href="/posts">
+              글 목록
+            </Link>
+            <Link className="underline" href="/profile">
+              프로필
+            </Link>
+            <Button onClick={logout}>로그아웃</Button>
+          </div>
         </div>
-      </div>
+      ) : (
+        <>
+          <p>로그인 후 이용가능합니다</p>
+          <Link className="underline" href="/login">
+            로그인
+          </Link>
+        </>
+      )}
     </>
   )
 }
--- apps/web/app/(pages)/page.tsx
+++ apps/web/app/(pages)/page.tsx
@@ -2,28 +2,17 @@
 
 import { Page } from '@/(components)/layout/Page'
 import UserProfile from '@/(components)/user/UserProfile'
-import { useProfile } from '@repo/query/user'
-import Link from 'next/link'
+import { Loading } from '@repo/ui/Loading'
+import { Suspense } from 'react'
 
 const HomePage = () => {
-  const { profile } = useProfile()
-
   return (
     <>
       <Page.Header title="홈" />
       <Page.Layout>
-        <>
-          {profile.data ? (
-            <UserProfile />
-          ) : (
-            <>
-              <p>로그인 후 이용가능합니다</p>
-              <Link className="underline" href="/login">
-                로그인
-              </Link>
-            </>
-          )}
-        </>
+        <Suspense fallback={<Loading />}>
+          <UserProfile />
+        </Suspense>
       </Page.Layout>
     </>
   )
