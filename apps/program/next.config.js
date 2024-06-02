a4eaa370d33ea2b83a62537fb7551a75db63ab8a
kdnhyk <kdnhyk@gmail.com>
2024-06-02 22:22:43 +0900

    feat: Add image loader

--- apps/program/next.config.js
+++ apps/program/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ['@repo/ui'],
+  images: {
+    domains: [process.env.NEXT_PUBLIC_SUPABASE_URL],
+    loader: 'custom',
+    loaderFile: './supabase-image-loader.ts',
+  },
   output: 'export',
   ...(process.env.NODE_ENV === 'production' && {
     compiler: {
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ['@repo/ui'],
+  images: {
+    domains: [process.env.NEXT_PUBLIC_SUPABASE_URL],
+    loader: 'custom',
+    loaderFile: './supabase-image-loader.ts',
+  },
   ...(process.env.NODE_ENV === 'production' && {
     compiler: {
       removeConsole: {
