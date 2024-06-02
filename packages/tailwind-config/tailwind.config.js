b9559e942519d27d0c3dd5761b678840c19f8560
kdnhyk <kdnhyk@gmail.com>
2024-06-02 22:17:04 +0900

    mod: Update tailwind config

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -2,22 +2,6 @@
 module.exports = {
   content: [],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      white: '#ffffff',
-      black: '#222222',
-      grey: {
-        100: '#F9F9F9',
-        300: '#F4F4F4',
-        500: '#ECECEC',
-        700: '#D2D2D2',
-        900: '#8E8E8E',
-      },
-      red: '#FF5C5C',
-      blue: '#0000FF',
-      green: '#00FF00',
-      yellow: '#FFFF00',
-    },
     extend: {},
   },
   plugins: [],
--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -3,22 +3,6 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      white: '#ffffff',
-      black: '#222222',
-      grey: {
-        '100': '#F9F9F9',
-        '300': '#F4F4F4',
-        '500': '#ECECEC',
-        '700': '#D2D2D2',
-        '900': '#8E8E8E',
-      },
-      red: '#FF5C5C',
-      blue: '#0000FF',
-      green: '#00FF00',
-      yellow: '#FFFF00',
-    },
     extend: {},
   },
   plugins: [],
