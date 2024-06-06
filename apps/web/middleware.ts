c5438c2e17264344d6f21bd41495c8f0db994167
kdnhyk <kdnhyk@gmail.com>
2024-06-06 19:41:42 +0900

    mod(web): Apply server client

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,27 +1,9 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { cookies } from 'next/headers'
-import { type CookieOptions, createServerClient } from '@supabase/ssr'
+import { createClient } from '@/utils/supabase/server'
 
 export async function middleware(req: NextRequest) {
-  const cookieStore = cookies()
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({ name, value, ...options })
-        },
-        remove(name: string, options: CookieOptions) {
-          cookieStore.delete({ name, ...options })
-        },
-      },
-    },
-  )
+  const supabase = createClient()
 
   const {
     data: { session },
@@ -33,5 +15,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/profile', '/posts:path*'],
+  matcher: ['/profile', '/post/new'],
 }
