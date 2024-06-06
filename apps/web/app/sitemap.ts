08a8b92c50567b904188d9a3c5fe5043d2f7fb3b
kdnhyk <kdnhyk@gmail.com>
2024-06-06 19:42:15 +0900

    mod(web): Configure posts

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -1,13 +1,24 @@
+import supabase from '@repo/supabase'
 import { MetadataRoute } from 'next'
 
 const URL = 'https://01.works'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const routes = ['', '/posts'].map((route) => ({
+  const routes = [''].map((route) => ({
     url: `${URL}${route}`,
+    lastModified: new Date(),
+    priority: 0.2,
+  }))
+
+  const { data: posts } = await supabase.from('post').select('id')
+
+  if (!posts) return routes
+
+  const postRoutes = posts.map(({ id }) => ({
+    url: `${URL}/post/${id}`,
     lastModified: new Date(),
     priority: 0.1,
   }))
 
-  return [...routes]
+  return [...routes, ...postRoutes]
 }
