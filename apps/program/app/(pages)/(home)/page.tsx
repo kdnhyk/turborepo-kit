5e3d86fa8a03911ef93bebcba5be9111432ce3dd
kdnhyk <kdnhyk@gmail.com>
2024-06-08 00:49:30 +0900

    mod(program): Apply RCC

--- apps/program/app/(pages)/(home)/page.tsx
+++ apps/program/app/(pages)/(home)/page.tsx
@@ -1,45 +1,13 @@
 import PostMenu from '@/(components)/common/PostMenu'
 import PostPageList from '@/(components)/list/PostPageList'
-import { POST_SELECTOR } from '@repo/api/post'
-import supabase from '@repo/supabase'
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from '@tanstack/react-query'
 
-export default async function HomePage() {
-  const queryClient = new QueryClient()
-
-  await queryClient.prefetchInfiniteQuery({
-    queryKey: ['post_page'],
-    queryFn: async ({ pageParam }) => {
-      let isLast = false
-      const { data, error } = await supabase
-        .from('post')
-        .select(POST_SELECTOR)
-        .range(0, 11)
-        .order('created_at', { ascending: false })
-
-      if (error) {
-        console.log(error)
-      }
-
-      if (!data || data.length < 12) {
-        isLast = true
-      }
-
-      return { result: data, currentPage: pageParam, isLast }
-    },
-    initialPageParam: 0,
-  })
-
+export default function HomePage() {
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
+    <>
       <PostMenu />
       <ul className="flex flex-1 flex-col divide-y">
         <PostPageList />
       </ul>
-    </HydrationBoundary>
+    </>
   )
 }
