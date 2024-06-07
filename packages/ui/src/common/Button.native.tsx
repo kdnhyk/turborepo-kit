40aa6df749b433120f4768b5e611481656c8e593
kdnhyk <kdnhyk@gmail.com>
2024-06-08 00:38:24 +0900

    mod: Update button ui

--- packages/ui/src/common/Button.native.tsx
+++ packages/ui/src/common/Button.native.tsx
@@ -1,21 +1,60 @@
-import { Pressable, Text } from 'react-native'
+import { Pressable, Text, StyleSheet } from 'react-native'
 
 interface ButtonProps {
   children: React.ReactNode
-  onclick?: () => void
+  onClick?: () => void
   disable?: boolean
   color?: 'black' | 'white'
 }
 
 export const Button = ({
   children,
-  onclick,
+  onClick,
   disable = false,
   color = 'white',
 }: ButtonProps) => {
   return (
-    <Pressable className="bg-black p-2" disabled={disable} onPress={onclick}>
-      <Text className="bg-green text-[20px]">{children}</Text>
+    <Pressable
+      style={color === 'white' ? styles.button_white : styles.button_black}
+      disabled={disable}
+      onPress={onClick}
+    >
+      <Text style={color === 'white' ? styles.text_white : styles.text_black}>
+        {children}
+      </Text>
     </Pressable>
   )
 }
+
+const styles = StyleSheet.create({
+  button_white: {
+    width: 'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    backgroundColor: 'white',
+    borderWidth: 1,
+  },
+  button_black: {
+    width: 'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    backgroundColor: 'black',
+    borderWidth: 1,
+  },
+  text_white: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  text_black: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+})
