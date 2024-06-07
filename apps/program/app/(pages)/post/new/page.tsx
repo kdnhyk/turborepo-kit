31a718aabaaf9d820fbc0065428d367679bf24af
kdnhyk <kdnhyk@gmail.com>
2024-06-08 00:49:52 +0900

    mod(program): Add exception

--- apps/program/app/(pages)/post/new/page.tsx
+++ apps/program/app/(pages)/post/new/page.tsx
@@ -1,6 +1,20 @@
-import PostForm from '@/(components)/form/PostForm'
+'use client'
 
-export default async function NewPage() {
+import PostForm from '@/(components)/form/PostForm'
+import useSession from '@/hooks/use-session'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function NewPage() {
+  const { session } = useSession()
+  const { replace } = useRouter()
+
+  useEffect(() => {
+    if (!session) {
+      replace('/login')
+    }
+  }, [session])
+
   return (
     <>
       <PostForm />
