b9bd07183235443b0508f0cc0f1b22055ab18293
kdnhyk <kdnhyk@gmail.com>
2024-06-12 18:34:12 +0900

    mod: Update language ko -> en

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -5,11 +5,11 @@ export default function NotFound() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center gap-2">
       <div className="flex flex-col items-center">
-        <p className="text-sm text-zinc-500">Not found page</p>
-        <h2>찾을 수 없는 페이지입니다</h2>
+        <p className="text-sm text-zinc-500">404 Error</p>
+        <h2>Not found page</h2>
       </div>
       <Link href="/">
-        <Button>홈으로</Button>
+        <Button>Back to Home</Button>
       </Link>
     </div>
   )
