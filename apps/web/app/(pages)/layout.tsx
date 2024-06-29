564a000805d38d66e799c37898448bff3a1d1131
kdnhyk <kdnhyk@gmail.com>
2024-06-30 02:16:26 +0900

    mod: Add profile hydration

--- apps/web/app/(pages)/layout.tsx
+++ apps/web/app/(pages)/layout.tsx
@@ -1,6 +1,13 @@
 import Header from '@/(components)/common/Header'
 import AuthProvider from '@/providers/auth-provider'
+import { createClient } from '@/utils/supabase/server-component'
+import { PROFILE_SELECTOR } from '@repo/api/user'
 import { QueryProvider } from '@repo/query/provider'
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from '@tanstack/react-query'
 
 export default async function Layout({
   children,
@@ -9,13 +16,39 @@ export default async function Layout({
   children: React.ReactNode
   modal: React.ReactNode
 }) {
+  const queryClient = new QueryClient()
+  const supabase = createClient()
+
+  const userId = (await supabase.auth.getSession()).data.session?.user.id
+
+  if (userId) {
+    await queryClient.prefetchQuery({
+      queryKey: ['profile_self'],
+      queryFn: async () => {
+        const { data, error } = await supabase
+          .from('profile')
+          .select(PROFILE_SELECTOR)
+          .eq('user_id', userId)
+          .single()
+
+        if (error) {
+          throw new Error('프로필 정보를 가져오는데 실패했습니다')
+        }
+
+        return data
+      },
+    })
+  }
+
   return (
     <QueryProvider>
-      <Header />
-      <AuthProvider>
-        <main>{children} </main>
-      </AuthProvider>
-      {modal}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Header />
+        <AuthProvider>
+          <main>{children}</main>
+        </AuthProvider>
+        {modal}
+      </HydrationBoundary>
     </QueryProvider>
   )
 }
