fbb1627f891bf00c12e122d538f506c6f0cb5ea0
kdnhyk <kdnhyk@gmail.com>
2024-06-30 02:15:24 +0900

    mod: Update supabase/ssr

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,16 +1,40 @@
+import { createServerClient } from '@supabase/ssr'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { createClient } from '@/utils/supabase/server'
 
-export async function middleware(req: NextRequest) {
-  const supabase = createClient()
+export async function middleware(request: NextRequest) {
+  let supabaseResponse = NextResponse.next({
+    request,
+  })
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll()
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value),
+          )
+          supabaseResponse = NextResponse.next({
+            request,
+          })
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options),
+          )
+        },
+      },
+    },
+  )
 
   const {
     data: { session },
   } = await supabase.auth.getSession()
 
   if (session?.user.role !== 'authenticated') {
-    return NextResponse.redirect(new URL('/login', req.url))
+    return NextResponse.redirect(new URL('/login', request.url))
   }
 }
 
