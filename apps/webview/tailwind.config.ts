fdc78a3ccae5ef995c514789a9eb63d874b026c1
kdnhyk <kdnhyk@gmail.com>
2024-07-01 00:46:12 +0900

    del: Replace webview to web

--- apps/webview/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'tailwindcss'
-import sharedConfig from '@repo/tailwind-config'
-
-const config: Pick<Config, 'presets' | 'content'> = {
-  presets: [sharedConfig],
-  content: ['./app/**/*.{tsx,mdx}', './node_modules/@repo/ui/**/*.tsx'],
-}
-export default config
