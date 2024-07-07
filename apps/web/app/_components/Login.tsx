5700330c5d1794d90c712508d10b919eb5113250
kdnhyk <kdnhyk@gmail.com>
2024-07-08 03:03:55 +0900

    mod: Update components

--- apps/web/app/_components/Login.tsx
+++ apps/web/app/_components/Login.tsx
@@ -1,16 +1,19 @@
 'use client'
 
+import { useHistory } from '@/_hooks/use-history'
 import { signInWithOAuth } from '@repo/api/auth'
 import { Button } from '@repo/ui/Button'
 
 export default function Login() {
+  const { history } = useHistory()
+
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex items-center justify-center py-10">
       <Button
         onClick={() =>
           signInWithOAuth('google', {
             redirectTo:
-              window.location.origin + `/auth?next${window.location.pathname}`,
+              window.location.origin + `/auth?next=${history?.at(-2)}`,
           })
         }
       >
