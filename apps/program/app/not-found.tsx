b915b1b7cd1f66ee2cedcea151f2a407d6f79214
kdnhyk <kdnhyk@gmail.com>
2024-07-08 03:05:15 +0900

    mod: Update settings

--- apps/program/app/not-found.tsx
+++ apps/program/app/not-found.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center gap-2">
+    <div className="flex flex-1 flex-col items-center justify-center gap-2 border bg-white">
       <div className="flex flex-col items-center">
-        <p className="text-sm text-zinc-500">404 Error</p>
-        <h2>Not found page</h2>
+        <p className="text-zinc-500">404 Error</p>
+        <h2 className="text-lg">Not found page</h2>
       </div>
       <Link href="/">
         <Button>Back to Home</Button>
