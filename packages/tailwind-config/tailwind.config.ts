b3099c8eb02bdd7f9fad3f9c71d4d1d73d8da103
kdnhyk <kdnhyk@gmail.com>
2024-07-08 02:43:26 +0900

    mod: Add animation

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -3,7 +3,30 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['var(--inter)'],
+        notoSans: ['var(--notoSans)'],
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        path: {
+          '0%': {
+            strokeDashoffset: '100',
+          },
+          '100%': {
+            strokeDashoffset: '0',
+          },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        dash: 'path 1s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
