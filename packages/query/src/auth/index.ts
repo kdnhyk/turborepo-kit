bfcb056fef82f889ebc75c19b44d95becc0929ab
kdnhyk <kdnhyk@gmail.com>
2024-07-09 02:44:22 +0900

    mod: Update logout

--- packages/query/src/auth/index.ts
+++ packages/query/src/auth/index.ts
@@ -1,14 +1,12 @@
 import { signout } from '@repo/api/auth'
 import { useQueryClient } from '@tanstack/react-query'
-import { profileQueryKey, userQueryKey } from '../user'
 
 export const useAuth = () => {
   const queryClient = useQueryClient()
 
   const logout = async () => {
     await signout()
-    queryClient.setQueryData(profileQueryKey.profile_self, () => null)
-    queryClient.setQueryData(userQueryKey.user, () => null)
+    queryClient.resetQueries()
   }
 
   return { logout }
