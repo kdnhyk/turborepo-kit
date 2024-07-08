e7a6d13d94fc40ffcd8398625994b7f36cb94164
kdnhyk <kdnhyk@gmail.com>
2024-07-09 01:08:25 +0900

    feat: Allow cors

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -15,6 +15,27 @@ module.exports = {
     loader: 'custom',
     loaderFile: '../../supabase-image-loader.ts',
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'http://localhost:3001',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ]
+  },
   ...(process.env.NODE_ENV === 'production' && {
     compiler: {
       removeConsole: {
