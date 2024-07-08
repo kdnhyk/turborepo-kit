f11a87fc93ac4c8ef79513b9f7fa355a4a49558e
kdnhyk <kdnhyk@gmail.com>
2024-07-09 02:47:24 +0900

    mod: Update login

--- apps/program/app/(pages)/@modal/(.)login/page.tsx
+++ apps/program/app/(pages)/@modal/(.)login/page.tsx
@@ -1,8 +1,11 @@
 'use client'
 
-import Login from '@/_components/Login'
+// import Login from '@/_components/Login'
 import { Modal } from '@repo/ui/Modal'
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/navigation'
+
+const Login = dynamic(() => import('@/_components/Login'), { ssr: false })
 
 export default function LoginModal() {
   const { back } = useRouter()
--- apps/program/app/_components/Login.tsx
+++ apps/program/app/_components/Login.tsx
@@ -1,7 +1,75 @@
 'use client'
 
-import useSignin from '@/_hooks/use-signin'
 import { Button } from '@repo/ui/Button'
+
+import { useEffect, useState } from 'react'
+import { invoke } from '@tauri-apps/api'
+import { listen } from '@tauri-apps/api/event'
+import { open } from '@tauri-apps/api/shell'
+import supabase from '@repo/supabase'
+import { signInWithOAuth } from '@repo/api/auth'
+import { Provider } from '@supabase/supabase-js'
+
+function getLocalHostUrl(port: number) {
+  return `http://localhost:${port}`
+}
+
+export function useSignin() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [port, setPort] = useState<number | null>(null)
+
+  useEffect(() => {
+    console.log('Refresh', port)
+    if (port) return
+
+    const unlisten = listen('oauth://url', (data) => {
+      setPort(null)
+      if (!data.payload) return
+
+      const url = new URL(data.payload as string)
+      const code = new URLSearchParams(url.search).get('code')
+
+      console.log('here', data.payload, code)
+      if (code) {
+        supabase.auth.exchangeCodeForSession(code).then(({ error }) => {
+          if (error) {
+            alert(error.message)
+            console.error(error)
+            return
+          }
+          location.reload()
+        })
+      }
+    })
+
+    let _port: number | null = null
+    invoke('plugin:oauth|start').then(async (port) => {
+      setPort(port as number)
+      _port = port as number
+    })
+    ;() => {
+      unlisten?.then((u) => u())
+      invoke('plugin:oauth|cancel', { port: _port })
+    }
+  }, [port])
+
+  const signinWithOauth = async (provider: Provider) => {
+    setIsLoading(true)
+
+    const { data, error } = await signInWithOAuth(provider, {
+      skipBrowserRedirect: true,
+      redirectTo: getLocalHostUrl(port!),
+    })
+
+    if (data.url) {
+      await open(data.url)
+    } else {
+      alert(error?.message)
+    }
+  }
+
+  return { signinWithOauth, isLoading }
+}
 
 export default function Login() {
   const { signinWithOauth } = useSignin()
