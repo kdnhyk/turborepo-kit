b37a293248fa7ea55c61b791026b352d2e1f7387
kdnhyk <kdnhyk@gmail.com>
2024-07-09 02:43:46 +0900

    feat: Configure baseUrl

--- packages/query/src/get-base-url.ts
+++ packages/query/src/get-base-url.ts
@@ -3,11 +3,18 @@
 import { isServer } from '@tanstack/react-query'
 
 export function getBaseURL() {
+  // Tauri
+  if (typeof window !== 'undefined' && window.__TAURI__) {
+    return 'https://nogwon.com'
+  }
+  // Default(browser)
   if (!isServer) {
     return ''
   }
+  // Server
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`
   }
+  // Localhost
   return 'http://localhost:3000'
 }
