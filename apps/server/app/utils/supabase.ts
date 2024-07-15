5ce1f5f17abab97b02d662ea6040d9343e1c6b77
kdnhyk <kdnhyk@gmail.com>
2024-07-15 19:29:12 +0900

    mod: Update env

--- apps/server/app/utils/supabase.ts
+++ apps/server/app/utils/supabase.ts
@@ -2,13 +2,13 @@ import { Database } from '@repo/supabase/database'
 import { createClient as _createClient } from '@supabase/supabase-js'
 
 export const supabase = _createClient<Database>(
-  useRuntimeConfig().supabaseUrl,
-  useRuntimeConfig().supabaseAnonKey,
+  process.env.NITRO_SUPABASE_URL,
+  process.env.NITRO_SUPABASE_ANON_KEY,
 )
 
 export const createClient = (authHeader: string) =>
   _createClient<Database>(
-    useRuntimeConfig().supabaseUrl,
-    useRuntimeConfig().supabaseAnonKey,
+    process.env.NITRO_SUPABASE_URL,
+    process.env.NITRO_SUPABASE_ANON_KEY,
     { global: { headers: { Authorization: authHeader } } },
   )
