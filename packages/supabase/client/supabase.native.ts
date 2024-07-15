ba37c72c54dc2586b681ce406d77b569c9cc7cdd
kdnhyk <kdnhyk@gmail.com>
2024-07-15 18:53:51 +0900

    mod: Update supabase client

--- packages/supabase/client/supabase.native.ts
+++ packages/supabase/client/supabase.native.ts
@@ -1,3 +1,5 @@
+// Expo
+
 import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
--- packages/supabase/client/supabase.ts
+++ packages/supabase/client/supabase.ts
@@ -1,7 +1,10 @@
+// Nextjs(server) & Nitro
+
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '../database.types'
 
 export default createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  (process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.NITRO_SUPABASE_URL)!,
+  (process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! ||
+    process.env.NITRO_SUPABASE_ANON_KEY)!,
 )
